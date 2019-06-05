cca4cecf1ed705140265fa324940494380ef60e7
Shodai Shibano <shodai@Shodaipc.local>
2019-06-05 23:40:51 +0900

    work in home

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd07e9905e5b6788f7b8ee0003c0be1beb277ffd298c3cb799f9df84f4401d3294f9a5ccf9c0a4dba1a7caad1018d9db0fc962efd141353311b62b7bd59e9feac'
